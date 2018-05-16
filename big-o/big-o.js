620b9d0342d5d167818b7985f438f7c284987bb1
joseph-nxtek <joseph@nxtek.co>
2018-05-15 23:59:06 -0700

    upload

---
+++ big-o/big-o.js
@@ -0,0 +1,15 @@
+// Big O Notation / Asymptonic Runtime - describe the efficient of algorithms
+
+// Time Complexity - File size vs Time 
+
+    // Electronic Transfer = O(s) as file size increase as does time.
+
+    // 
+
+// Space Complexity - RAM and Space vs Time
+
+// Big O
+
+// Big Omega
+
+// Big Theta
